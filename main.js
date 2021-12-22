b669d7cf092243459100ab03dd6e08a83f1e7a22
tomdichmont <t.dichmont@gmail.com>
2021-12-22 16:07:46 +0000

    Tasks 1 + 2 complete

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 /* 
 👉 Write your kata here!
+
+A knight needs to defeat an army of giants.  He needs to fire 3 arrows into the each giant to kill it.  
+
+How many arrows does the knight need in his quiver to defeat an army of a given size?
+
 */
 
+
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+
+function battleOutcome (giantsNumber, arrowNumber){
+    if (arrowNumber === giantsNumber*3){
+        return true;
+    }
+    else if(arrowNumber > giantsNumber*3){return true;}
+    else {return false;};
+
+
+
+// if arrowNumber is 3 times the number of giantsNumber, then return true.
+
+// if arrowNumber is not 3 times the giantsNumber, return false + message?
+
+}
+
+export default battleOutcome;
--- main.test.js
+++ main.test.js
@@ -1,1 +1,38 @@
 //👉 Write your tests below here:
+
+//import function 
+
+import battleOutcome from "./main.js";
+
+// Arrange 
+
+// expected can be true or false 
+
+// actual should be true or false
+
+// test examples: 
+
+//  if giantsNumber = 3 then arrowNumber should be 9, then return true;
+
+// if giantsNumber = 3 and arrowNumber = 8, then return false;
+
+// if giantsNumber = 3 and arrowNumber = 10, then return true;
+
+// if giantsNumber != Number 
+
+// Act 
+
+// Assert 
+
+
+test.each([
+    //variable of the input called letter, each input should be from a to z
+    {giantsNumber:3, arrowNumber:9, expected: true},
+    {giantsNumber:3, arrowNumber:8, expected: false},
+    {giantsNumber:3, arrowNumber:10, expected: true},
+
+])("you should have enough arrows to kill the giants", function({ giantsNumber, arrowNumber, expected}) {
+    expect(battleOutcome(giantsNumber, arrowNumber)).toBe(expected);
+})
+
+
